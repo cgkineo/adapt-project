6b9627e69f58285c7f50e395419143efd3956df8
Oliver Foster <oliver.foster@kineo.com>
2026-03-10 23:20:48 +0000

    Add helper functions

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import Data from './lib/Data.js'
 import Plugins from './lib/Plugins.js'
 import Schemas from './lib/Schemas.js'
 import Translate from './lib/Translate.js'
+import { SchemaError } from 'adapt-schemas'
 
 export {
   Framework,
   Data,
   Plugins,
   Schemas,
-  Translate
+  Translate,
+  SchemaError
 }
